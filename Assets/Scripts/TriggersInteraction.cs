f77ba3d46323e8ede5f04c78b05d8a2c718f1130
denichirich <deni.chirich@gmail.com>
2020-11-03 12:32:40 -0300

    3-11

--- Assets/Scripts/TriggersInteraction.cs
+++ Assets/Scripts/TriggersInteraction.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 [RequireComponent(typeof(Collider))]
 public class TriggersInteraction : MonoBehaviour
 {
-    [Header("Configurable en Project Settings")]
-    public string interactionAxis = "Interact";
+    [Header("Configurable por codigo via opciones")]
+    public KeyCode interactionKey = KeyCode.E;
     [Header("Rango para interactuar con este objeto")]
     public Collider colTriggerEnabled;
 
@@ -17,30 +17,42 @@ public class TriggersInteraction : MonoBehaviour
     private bool active = false;
     private bool wasActivated = false;
 
+    [Header("Instanciar uno o mas objetos con eventos")]
+    public List<GameObject> prefabInteraccion;
 
     private void OnTriggerStay(Collider other)
     {
+        if (other.gameObject.layer != GeneralInfo.PLAYER_LAYER || wasActivated) //evita repetir
+            return;
+
         RaycastHit auxObstaculos;
 
-        var rayCanInteract = new Ray(transform.position, other.transform.position); //, out auxObstaculos);
-        Debug.DrawRay(rayCanInteract.origin, rayCanInteract.direction, Color.red);
-        //Physics.Raycast(rayCanInteract, out auxObstaculos,);
+        //evito que haya un obstaculo en el medio para poder interactuar
 
-        Debug.Log(Physics.Raycast(rayCanInteract, out auxObstaculos));
-        Debug.Log(auxObstaculos.collider.name);
+        var rayCanInteract = new Ray(transform.position, other.transform.position);
+        var rayval = Physics.Raycast(rayCanInteract, out auxObstaculos, GeneralInfo.TERRAIN_LAYER);
 
-        //if (auxObstaculos.transform)
-        //{
-        //    Debug.Log(canInteract && other.gameObject.layer == GeneralInfo.PLAYER_LAYER);
-        //}
+        //Debug.Log(auxObstaculos);
+        //print(rayval);
+
+        if (Input.GetKeyDown(interactionKey) && auxObstaculos.collider == null)
+        {
+            Debug.Log("Todo ok, no hay obstaculos, toque el input");
+
+            NarrativeManager.instance.OnPrepairForInteraction(); // desactivo camara, movimiento, etc
+            GameObject container = Instantiate<GameObject>(new GameObject(), Vector3.zero, Quaternion.identity);
+            foreach (var item in prefabInteraccion)
+            {
+                Instantiate(item, transform.position, Quaternion.identity, container.transform);
+                // nota:
+                // necesito un script para guardar las posiciones y datos de camara?
+                // estarian en todos los prefabs que quieran crear
+            }
+            active = wasActivated = true;
+            this.enabled = false;
+        }
 
 
-        // Si se mantiene dentro del rango del trigger, y es el jugador
-        //if (other.gameObject.layer == GeneralInfo.PLAYER_LAYER)
-        //{
-        //    Debug.Log("Interactuando con " + this.name);
-        //    active = true;
-        //    wasActivated = true;
-        //}
+
     }
 }
